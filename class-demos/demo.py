ef860e2e6ca071ff9ca5ba6025aafbffa326a617
NikolaiMilburn <nicholasgtmilburn@gmail.com>
2021-10-27 11:27:25 -0600

    fixed venv

--- class-demos/demo.py
+++ class-demos/demo.py
@@ -1,19 +1,43 @@
 import psycopg2
 
-connection = psycopg2.connect('dbname=example')
+connection = psycopg2.connect('dbname=example user=postgres host=localhost password=postgres')
 
-curser = connection.curser()
+cursor = connection.cursor()
 
-curser.execute('''
-CREATE TABLE table2 (
-    id INTERGER PRIMARY KEY,
-    completed BOOLEAN NOT NULL DEFAULT False
+cursor.execute("DROP TABLE IF EXISTS table2;")
+
+cursor.execute('''
+    CREATE TABLE table2 (
+        id INTEGER PRIMARY KEY,
+        completed BOOLEAN NOT NULL DEFAULT False
     );
-    ''')
+''')
 
-curser.execute('INSERT INTO table2 (id, completed) VALUES(1, true);')
+cursor.execute('INSERT INTO table2 (id, completed) VALUES(%s, %s);', (1, True))
+
+SQL = 'INSERT INTO table2 (id, completed) VALUES (%(id)s, %(completed)s);'
+data = {
+    'id': 2,
+    'completed': False,
+}
+cursor.execute(SQL, data)
+
+data = {
+    'id': 3,
+    'completed': False,
+}
+cursor.execute(SQL, data)
+
+cursor.execute('SELECT * from table2;')
+
+result = cursor.fetchmany(2)
+print(result)
+
+result2 = cursor.fetchone()
+print(result2)
+
 
 connection.commit()
 
 connection.close()
-curser.close()
+cursor.close() 
